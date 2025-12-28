8a49688d4e3bce826cc7d8f19da6f2148e78a021
user.email <sunny@sunnydeMacBook-Pro.local>
2025-12-28 21:25:25 +0800

    提交测试

--- merchant-admin/src/main/java/com/nageoffer/onecoupon/merchant/admin/service/impl/CouponTaskServiceImpl.java
+++ merchant-admin/src/main/java/com/nageoffer/onecoupon/merchant/admin/service/impl/CouponTaskServiceImpl.java
@@ -238,4 +238,10 @@ public class CouponTaskServiceImpl extends ServiceImpl<CouponTaskMapper, CouponT
                     });
         }
     }
+    // 提交测试
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
